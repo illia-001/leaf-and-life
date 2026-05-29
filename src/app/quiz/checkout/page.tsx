cc4d557751d906a7a9860b1086cc663ac165f38e
Illia Tryndiuk <dedmoroz@Deds-MacBook-Pro.local>
2026-05-29 19:34:17 +0300

    add redirect from checkout to quiz page if answers are missing

--- src/app/quiz/checkout/page.tsx
+++ src/app/quiz/checkout/page.tsx
@@ -3,8 +3,23 @@
 import AnimateItems from "@/components/shared/animateItems";
 import SubscriptionPlanList from "@/components/features/checkout/SubscriptionPlanList";
 import UserForm from "@/components/features/checkout/Forms/userForm";
+import { useEffect } from "react";
+import { useQuiz } from "@/hooks/useQuiz";
+import { ROUTES } from "@/constants/Routing";
+import { useRouter } from "next/navigation";
 
 export default function Checkout() {
+  const { answers } = useQuiz();
+  const router = useRouter();
+
+  const hasAnswers = answers && Object.keys(answers).length > 0;
+
+  useEffect(() => {
+    if (!hasAnswers) {
+      router.replace(ROUTES.QUIZ);
+    }
+  }, [router, hasAnswers]);
+
   return (
     <div className="z-1 bg-primary flex flex-col items-center gap-10 px-4 py-8">
       <div className="flex flex-col gap-4 col-span-full">
